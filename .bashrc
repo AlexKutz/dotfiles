861448ec7be26da905d8f1c7591bbf183e4d4eb4
kurt <kurtzalexo@gmail.com>
2025-03-21 20:41:48 +0200

    update

--- .bashrc
+++ .bashrc
@@ -1,13 +1,42 @@
 #
 # ~/.bashrc
 #
-
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
-alias ls='ls --color=auto'
+# Print whether
+curl wttr.in/Stara+Syniava?format="%l:+%c+%t+(%f)+%w\n"
+
+# ls aliaces
+alias ls='ls -Fh --color=auto'
+alias lsa="ls -a"
+alias ll="ls -l"
+alias lla="ll -a"
+
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 
 alias vim=nvim
+
 alias dotfiles='git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+
+alias poweroff="sudo poweroff"
+alias reboot="sudo reboot"
+
+alias feh="feh"
+
+# Отображать битые сылки красным в ls
+export LS_COLORS="$LS_COLORS:or=31"
+
+# Culr weather
+# https://github.com/chubin/wttr.in
+alias wttr='curl -s "wttr.in/Stara+Syniava?lang=ru" \
+   | grep -v "New feature" \
+   | grep -v Follow'
+
+alias wttr2='curl -s "v2d.wttr.in/Stara+Syniava" \
+   | grep -v "New feature" \
+   | grep -v Follow'
+
+# https://github.com/nvbn/thefuck
+eval $(thefuck --alias)
--- install.sh
+++ install.sh
@@ -1,1 +1,64 @@
-pacman -S ttf-jetbrains-mono 
+#!/bin/bash
+
+log_message() {
+    echo "[INFO] $1"
+}
+
+error_message() {
+    echo "[ERROR] $1"
+}
+
+update_system() {
+	log_message "System updating..."
+	pacman -Syu --noconfirm
+}
+
+function install_package {
+	log_message "Installing: $1"
+	pacman -S --noconfirm "$1" || {
+		error_message "Error"
+		return 1
+	}
+}
+
+main() {
+	echo "alex ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot" | sudo tee /etc/sudoers.d/sudo-poweroff
+
+	update_system()
+	install_package sudo
+
+	# font for terminal and status bar
+	install_package "ttf-jetbrains-mono"
+
+	# font with icons
+	install_package ttf-font-awesome
+
+	# better i3 status bar
+	install_package i3blocks
+
+	# for easier battery charge display
+	install_package acpi
+
+	# for open images
+	install_package feh
+
+	# package for mount android fs
+	install_package android-file-transfer
+	
+	install_package navi
+	install_package thefuck
+	install_package fastfetch
+
+	curl -sS https://raw.githubusercontent.com/ekkinox/yai/main/install.sh | bash
+
+	git clone https://github.com/Hendrikto/backlight_control.git
+	cd backlight_control
+	sudo make install
+
+	# https://github.com/chubin/cheat.sh
+	curl -s https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
+
+}
+
+
+# add swap file
